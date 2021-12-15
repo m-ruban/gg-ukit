4d821edb1f3d03975b7c566ff1d3c4fc4f60bd4a
Ruban Mikhail <m.ruban@hh.ru>
2021-12-15 19:48:13 +0300

    tooltip

---
+++ src/components/Tooltip/index.tsx
@@ -0,0 +1,57 @@
+import React, { FC, ReactNode, useState, useRef, useEffect, useCallback } from 'react';
+import classnames from 'classnames';
+import 'components/Tooltip/tooltip.less';
+
+interface TooltipProps {
+    children: ReactNode;
+    view: ReactNode;
+}
+
+const Tooltip: FC<TooltipProps> = ({ children, view }) => {
+    const [show, setShow] = useState(false);
+    const tooltipRef = useRef<HTMLDivElement>(null);
+    const arrowRef = useRef<HTMLSpanElement>(null);
+    const wrapperRef = useRef<HTMLSpanElement>(null);
+
+    const updateTipPosition = useCallback(() => {
+        const tooltip = tooltipRef.current;
+        const arrow = arrowRef.current;
+        const wrapper = wrapperRef.current;
+        const tooltipWidth = wrapper.offsetLeft + tooltip.offsetWidth;
+        const clientWidth = window.document.documentElement.clientWidth;
+        if (tooltipWidth > clientWidth) {
+            tooltip.style.left = 'auto';
+            tooltip.style.right = '0px';
+            arrow.style.left = 'auto';
+            arrow.style.right = '10px';
+        } else {
+            tooltip.style.left = '0px';
+            tooltip.style.right = 'auto';
+            arrow.style.left = '10px';
+            arrow.style.right = 'auto';
+        }
+    }, []);
+
+    useEffect(() => {
+        if (show) {
+            updateTipPosition();
+        }
+    }, [updateTipPosition, show]);
+
+    return (
+        <span
+            ref={wrapperRef}
+            className="gg-tooltip-wrapper"
+            onMouseEnter={() => setShow(true)}
+            onMouseLeave={() => setShow(false)}
+        >
+            <div ref={tooltipRef} className={classnames('gg-tooltip', { 'gg-tooltip_visible': show })}>
+                <div>{view}</div>
+                <span ref={arrowRef} className="gg-tooltip-arrow" />
+            </div>
+            <span className="gg-tooltip-link">{children}</span>
+        </span>
+    );
+};
+
+export default Tooltip;
---
+++ src/stories/Tooltip.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import TooltipComponent from 'components/Tooltip';
+import Paragraph from 'components/Paragraph';
+
+export const Tooltip = (args) => (
+    <Paragraph>
+        Где-то влияние, великого отца ужасов, ощущается меньше, как в сериях Amnesia и Layers of Fear. А где-то можно
+        найти лишь лавкрафтовские крохи, например, в первом
+        <TooltipComponent view={<>Финальный босс игры - Шуб-Ниггурат, взят из произведений Лавкрафта</>} {...args}>
+            Quake
+        </TooltipComponent>
+        . Но, если рассматривать конкретно жанр игровых ужасов, то получается довольно мемная картинка. Где-то Лавкрафта
+        видно целиком, например, в Sinking City или в Call of Cthulhu.
+    </Paragraph>
+);
+
+export default {
+    title: 'Tooltip',
+    component: TooltipComponent,
+};
