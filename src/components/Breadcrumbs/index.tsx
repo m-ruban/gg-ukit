3075cc138fa67330e9bdf2d0c69bd847630ba6b6
Ruban Mikhail <m.ruban@hh.ru>
2022-08-01 00:23:01 +0300

    breadcrumbs home logic

--- src/components/Breadcrumbs/index.tsx
+++ src/components/Breadcrumbs/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import classnames from 'classnames';
 import Link, { LinkType } from 'gg-ukit/components/Link';
 import ArrowRight from 'gg-ukit/components/Icon/ArrowRight';
 import HomeAlt from 'gg-ukit/components/Icon/HomeAlt';
@@ -20,14 +21,18 @@ const Breadcrumbs: FC<BreadcrumbsProp> = ({ links, linkType }: BreadcrumbsProp) 
         {links.map(({ href, title }, index) => (
             <div key={`${href}_${index}`} className="gg-breadcrumbs-item">
                 {index === 0 && (
-                    <span className="gg-breadcrumbs-home">
-                        <HomeAlt color={Colors.Secondary} />
-                    </span>
+                    <a href={href}>
+                        <span className="gg-breadcrumbs-home">
+                            <HomeAlt color={Colors.Secondary} />
+                        </span>
+                    </a>
                 )}
                 {index !== 0 && <ArrowRight color={Colors.Secondary} />}
-                <Link href={href} type={linkType}>
-                    {title}
-                </Link>
+                <div className={classnames({ 'gg-breadcrumbs-link-hide-xs': index === 0 })}>
+                    <Link href={href} type={linkType}>
+                        <span className="gg-breadcrumbs-text">{title}</span>
+                    </Link>
+                </div>
             </div>
         ))}
     </div>
